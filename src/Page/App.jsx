be75d1611a5cb27ba7377bc28900b68762e05f12
Dung.QuocPhi <dung.quocphi@coxautoinc.com>
2021-03-28 22:10:45 +0700

    remove listen port

--- src/Page/App.jsx
+++ src/Page/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable global-require */
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Number from './Number/Number';
@@ -6,18 +5,6 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const http = require('http');
-  const port = process.env.PORT || 3000;
-
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-  });
-
-  server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
-  });
   const containerLayout = {
     span: 8,
     offset: 2,
